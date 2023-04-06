18a9a0431cc50857544e002b617a4db69f377828
rubenbouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-06 15:44:52 +0200

    Added comments

--- src/Webscraper_1.py
+++ src/Webscraper_1.py
@@ -6,17 +6,20 @@ from bs4 import BeautifulSoup
 import selenium
 import csv
 
+# Setup the url you want to visit
 url = 'https://www.bever.nl/c/heren/jassen/hardshell.html'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
 
+# Setup the header to act like a real browser
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+# Get the HTML contents
 response = requests.get(url, headers=headers)
-
 soup = BeautifulSoup(response.content, 'html.parser')
 
+# Get all the selected tags
 product_tags = soup.find_all('div', {'class': 'as-m-product-tile'})
 
+# Loop through the tags to find the content that you want
 with open('bever_products.csv', 'w', newline='') as csvfile:
     fieldnames = ['name', 'price', 'description']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
