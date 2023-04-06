90591dda4683f4cfdda6c8979f672dda24be615a
rubenbouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-06 14:58:14 +0200

    Added the ability to grab tags from the HTML file

--- src/Webscraper_1.py
+++ src/Webscraper_1.py
@@ -2,4 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-#test
+url = 'https://www.bever.nl/c/heren/jassen/hardshell.html'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+response = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+product_tags = soup.find_all('div', {'class': 'as-m-product-tile'})
+
