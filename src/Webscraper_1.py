7694b8ad361c1b7b91eba2ffcbf580ca2b039dfd
rubenbouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-06 15:15:22 +0200

    Added a disclaimer comment

--- src/Webscraper_1.py
+++ src/Webscraper_1.py
@@ -1,3 +1,6 @@
+# Webscraper for the website bever.nl to get images and reviews
+# Note: this program is specificaly made for an assignment for school and is not made to be used with any other intentions
+
 import requests
 from bs4 import BeautifulSoup
 import csv
