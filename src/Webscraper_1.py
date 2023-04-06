8de6179ce5363c49bdb3abbd046a7a5dba7a4587
rubenbouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-06 15:10:45 +0200

    Seperated everything in different methods

--- src/Webscraper_1.py
+++ src/Webscraper_1.py
@@ -2,24 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = 'https://www.bever.nl/c/heren/jassen/hardshell.html'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
+class WebscraperBever:
+    def makeConnection():
+        url = 'https://www.bever.nl/c/heren/jassen/hardshell.html'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        response = requests.get(url, headers=headers)
 
-response = requests.get(url, headers=headers)
+    def getTags(response):
+        soup = BeautifulSoup(response.content, 'html.parser')
+        product_tags = soup.find_all('div', {'class': 'as-m-product-tile'})
 
-soup = BeautifulSoup(response.content, 'html.parser')
+    def loopTroughTags(product_tags):
+        with open('bever_products.csv', 'w', newline='') as csvfile:
+            fieldnames = ['name', 'price', 'description']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-product_tags = soup.find_all('div', {'class': 'as-m-product-tile'})
-
-with open('bever_products.csv', 'w', newline='') as csvfile:
-    fieldnames = ['name', 'price', 'description']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for tag in product_tags:
-        name = tag.find('div', {'class': 'as-m-product-tile__title-wrapper'}).text.strip()
-        price = tag.find('div', {'class': 'as-a-price__value as-a-price__value--sell'}).text.strip()
-        description = tag.find('div', {'class': 'as-m-product-tile__info-wrapper'}).text.strip()
-        writer.writerow({'name': name, 'price': price, 'description': description})
+            writer.writeheader()
+            for tag in product_tags:
+                name = tag.find('div', {'class': 'as-m-product-tile__title-wrapper'}).text.strip()
+                price = tag.find('div', {'class': 'as-a-price__value as-a-price__value--sell'}).text.strip()
+                description = tag.find('div', {'class': 'as-m-product-tile__info-wrapper'}).text.strip()
+                writer.writerow({'name': name, 'price': price, 'description': description})
