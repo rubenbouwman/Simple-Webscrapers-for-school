86fb50fcd85593e3672e32a2fadbe9725eaacdfe
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-12 23:01:10 +0200

    Added the function to scrape data from all reviews

--- src/Webscraper_Bol.py
+++ src/Webscraper_Bol.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import csv
 
 # URL with the prefered product catagory and filter
-url_category = 'https://www.bol.com/nl/nl/s/?page=1&searchtext=outdoor+broek+heren&view=tile'
+url_category = 'https://www.bol.com/nl/nl/l/outdoorschoenen/39510/?page=1'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 # lijst maken om de links de producten op te slaan
@@ -34,6 +34,29 @@ with open('Output/Bol-product-reviews.csv', 'w', newline='') as csvfile:
 
             productName = productSoup.find("h1", { "class" : "page-heading" })
             productImage = productSoup.find("tagName", { "img" : "data-zoom-image-url" })
+
+            review_tags = productSoup.find_all('li', {'class': 'review js-review'})
+
+            for tag in review_tags:
+                # extract rating
+                score = tag.select_one('.star-rating')['aria-label']
+                
+                # extract review title
+                title = tag.select_one('.review__title').text.strip()
+                
+                # extract review body
+                body = tag.select_one('[data-test="review-body"]').text.strip()
+                body = body.encode('utf-8', 'ignore').decode('utf-8')
+                
+                # extract pros and cons
+                pros_list = tag.select('.review-pros-and-cons__list--pros li')
+                pros = [li.text.strip() for li in pros_list]
+                
+                cons_list = tag.select('.review-pros-and-cons__list--cons li')
+                cons = [li.text.strip() for li in cons_list]
+                
+                # write everything into the CSV file
+                writer.writerow({'product': productName, 'img': productImage, 'score': score, 'title': title, 'pros': pros, 'cons': cons, 'body': body})
 
 
 
