228ab27bf910201e53c971bb46610936a02a69c2
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-13 00:02:49 +0200

    added imports and disclaimer

---
+++ src/Webscraper_Decathlon.py
@@ -0,0 +1,7 @@
+# Webscraper for the website Decathlon.nl to get images and reviews
+# Note: this program is specificaly made for an assignment for school and is not made to be used with any other intentions
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
