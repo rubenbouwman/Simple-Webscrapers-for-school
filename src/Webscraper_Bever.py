dbd35c243f7e61660267f697e6395997a4cf4ff7
rubenbouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-04-12 15:48:48 +0200

    JSON files van de reviews worden nu opgevraagd

--- src/Webscraper_Bever.py
+++ src/Webscraper_Bever.py
@@ -4,6 +4,8 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import json
+from urllib.request import urlopen
 
 # De URL van de product categorie wat je wilt bezoeken
 url_category = 'https://www.bever.nl/c/heren/broeken/wandelbroeken.html'
@@ -11,11 +13,11 @@ url_category = 'https://www.bever.nl/c/heren/broeken/wandelbroeken.html'
 # Headers instellen voor de request
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-# Een lijst maken om de links van de producten op te slaan
+# lijsten maken om de links en codes van de producten op te slaan
 product_links = []
 product_codes = []
 
-# CSV-bestand openen om de productinformatie en beoordelingen op te slaan
+# CSV-bestand maken/openen om de productinformatie en beoordelingen op te slaan
 with open('bever_products.csv', 'w', newline='') as csvfile:
     fieldnames = ['name', 'score', 'pluspunt', 'minpunt']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -34,5 +36,13 @@ for tag in product_tags:
 
 # loop met behulp van de product code door alle reviews van alle producten
 for product_code in product_codes:
-    url = {f'https://widgets.reevoo.com/api/product_reviews?per_page=3&trkref=BEV&sku={product_code}&locale=nl-NL&display_mode=embedded&page=1'}
-    writer.writerow({'name': name, 'score': score, 'pluspunt': pluspunt, 'minpunt': minpunt})
+    response = requests.get(f'https://widgets.reevoo.com/api/product_reviews?per_page=3&trkref=BEV&sku={product_code}&locale=nl-NL&display_mode=embedded&page=1')
+    if response.status_code == 200:
+        json_data = response.json()
+
+        print(json_data)
+    else:
+        print('The request failed with status code', response.status_code)
+
+
+    # writer.writerow({'name': name, 'score': score, 'pluspunt': pluspunt, 'minpunt': minpunt})
